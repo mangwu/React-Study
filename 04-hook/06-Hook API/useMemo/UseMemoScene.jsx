1ac4115d0286494d595912f32a3011868b99b803
mangwu <1185956753@qq.com>
2021-08-25 17:00:39 +0800

    打包功能使用，serve库安装使用

--- 04-hook/06-Hook API/useMemo/UseMemoScene.jsx
+++ 04-hook/06-Hook API/useMemo/UseMemoScene.jsx
@@ -13,7 +13,9 @@ function TestWithoutMemo() {
   }
   return (
     <div>
-      <h4>每重新渲染就会执行expensive： {Expensive()} {"<="}函数返回的结果</h4>
+      <h4>
+        每重新渲染就会执行expensive： {Expensive()} {"<="}函数返回的结果
+      </h4>
       <div>
         count: {count}
         <button
@@ -37,4 +39,44 @@ function TestWithoutMemo() {
   );
 }
 
+function TestWithUseMemo() {
+  const [count, setCount] = useState(1);
+  const [val, setVal] = useState("");
+  const expensive = useMemo(() => {
+    console.log("expensive");
+    let sum = 0;
+    for (let i = 1; i <= count; i++) {
+      sum += i;
+    }
+    return sum;
+  }, [count]);
+  return (
+    <div>
+      <h4>
+        每重新渲染就会执行expensive： {expensive} {"<="}函数返回的结果
+      </h4>
+      <div>
+        count: {count}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          + 1
+        </button>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={val}
+          onChange={(e) => {
+            setVal(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
 ReactDOM.render(<TestWithoutMemo />, document.querySelector("#root"));
+ReactDOM.render(<TestWithUseMemo />, document.querySelector("#root2"));
+
