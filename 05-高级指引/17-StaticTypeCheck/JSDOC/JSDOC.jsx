618f1e8290b778cce84f78d9c6c8687e2366d6e6
mangwu <1185956753@qq.com>
2021-10-19 17:31:19 +0800

    statictypecheck

---
+++ 05-高级指引/17-StaticTypeCheck/JSDOC/JSDOC.jsx
@@ -0,0 +1,26 @@
+/**
+ * @description 使用JSDOC为JavaScript做静态类型检查
+ * @author mangwu <1185956753@qq.com>
+ * @version  1.0
+ * @date 2021-10-19 16:07:54
+ * @copyright © 2021 wangzhihao, All rights reserved.
+ */
+
+/**
+ * @description 使用JSDOC做类型检查
+ * @class JSDOCStaticTypeCheck
+ * @extends React.PureComponent
+ */
+class JSDOCStaticTypeCheck extends React.PureComponent {
+
+}
+
+/**
+ * @description test
+ * @function Test
+ * @param {object} props - 属性
+ * @returns {React.ReactElement} react元素
+ */
+function Test(props) {
+  // code
+}
