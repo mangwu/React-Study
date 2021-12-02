697e8abdd9a0a736277f631023c8855c5f052dac
mangwu <1185956753@qq.com>
2021-12-02 15:49:46 +0800

    ReactDOM.unountComponentAtNode

--- 07-React API/ReactDOM/unmountComponentNode/UnmountComponentNode.jsx
+++ 07-React API/ReactDOM/unmountComponentNode/UnmountComponentNode.jsx
@@ -6,9 +6,10 @@
  * @copyright © 2021 wangzhihao, All rights reserved.
  */
 const { useState, useRef, useEffect } = React;
-const div = document.createElement("div");
-div.setAttribute("id", "modal");
-document.body.appendChild(div);
+const div2 = document.createElement("div");
+div2.setAttribute("id", "modal3");
+div2.textContent = "通过createPortal挂载的组件"
+document.body.appendChild(div2);
 /**
  * @description App
  * @function App
@@ -18,23 +19,61 @@ document.body.appendChild(div);
 function App(props) {
   const [timer, setTimer] = useState(10);
   const spinRef = useRef(null);
+  const timerRef = useRef(null);
   useEffect(() => {
-    spinRef.current = setInterval(() => {
+    const div = document.createElement("div");
+    div.setAttribute("id", "modal2");
+    document.body.appendChild(div);
+    timerRef.current = setInterval(() => {
       setTimer((timer) => timer - 1);
     }, 1000);
+    ReactDOM.render(<antd.Spin />, document.querySelector("#modal"));
+    ReactDOM.render(<antd.Spin />, document.querySelector("#modal2"));
   }, []);
   useEffect(() => {
     if (timer === 0) {
+      console.log(spinRef.current);
       ReactDOM.unmountComponentAtNode(document.querySelector("#modal"));
-      clearInterval(spinRef.current);
+      ReactDOM.unmountComponentAtNode(document.querySelector("#modal2"));
+      ReactDOM.unmountComponentAtNode(document.querySelector("#modal3"));
+      ReactDOM.unmountComponentAtNode(spinRef.current);
+      clearInterval(timerRef.current);
     }
   });
   return (
     <React.Fragment>
       <h2>模拟卸载组件</h2>
       timer: {timer}
-      {ReactDOM.createPortal(<antd.Spin />, document.querySelector("#modal"))}
+      <div ref={spinRef}>
+        通过ref卸载的组件
+        <antd.Spin />
+      </div>
+      {ReactDOM.createPortal(<antd.Spin />, document.querySelector("#modal3"))}
     </React.Fragment>
   );
 }
+/**
+ * @description App
+ * @class App
+ * @extends React.Component
+ */
+// class App extends React.Component {
+//   /**
+//    * @description 构造函数
+//    * @method constructor
+//    * @param {object} props - 属性
+//    * @returns void
+//    */
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       timer: 10
+//     }
+//     this.spinRef = React.createRef(null);
+//   }
+//   componentDidMount() {
+//     const div = document.createElement('div');
+
+//   }
+// }
 ReactDOM.render(<App />, document.querySelector("#root"));
